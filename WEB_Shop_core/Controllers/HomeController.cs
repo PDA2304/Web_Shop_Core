c7b50d71e3041dc5577044645d253e292e46a8bc
isip_d.a.pahomov@mpt.ru <isip_d.a.pahomov@mpt.ru>
2020-10-09 15:05:24 +0300

    web_shop_core

---
+++ WEB_Shop_core/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WEB_Shop_core.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult Bedroom()
+        {
+            return View();
+        }
+        public ActionResult Children_Bedroom()
+        {
+            return View();
+        }
+        public ActionResult Kitchen()
+        {
+            return View();
+        }
+        public ActionResult Living_Room()
+        {
+            return View();
+        }
+
+        public ActionResult Product_card()
+        {
+            return View();
+        }
+
+        public ActionResult Personal_Account()
+        {
+            return View();
+        }
+
+
+    
+    }
+}
